663fe293006668180f9dbaaeda81978ef5312199
Looobay <69517054+Looobay@users.noreply.github.com>
2024-05-14 21:00:03 +0200

    test

--- code/lby/Components/Radio.cs
+++ code/lby/Components/Radio.cs
@@ -2,14 +2,28 @@ using Sandbox;
 
 public sealed class Radio : Component
 {
+	/// <summary>
+	/// You need to put here a sound event not a sound file !!
+	/// </summary>
+	[Property] public SoundEvent radioSound { get; set; }
+
+	/// <summary>
+	/// The volume.
+	/// </summary>
+	[Property] public float Volume { get; set; } = 1;
+	
+	/// <summary>
+	/// The pitch.
+	/// </summary>
+	[Property] public float Pitch { get; set; } = 1;
 	protected override void OnUpdate()
 	{
 	}
 
 	protected override void OnEnabled()
 	{
-		var sound = Sound.Play( "portal_radio_uncompressed", Transform.World.Position );
-		sound.Pitch = 1.0f;
-		sound.Volume = 2.5f;
+		var sound = Sound.Play( radioSound, Transform.World.Position );
+		sound.Pitch = Pitch;
+		sound.Volume = Volume;
 	}
 }
